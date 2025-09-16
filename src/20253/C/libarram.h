8235c52bf2997099acfc083f68e7907d2ff27ce6
Mario Alberto Ibarra Manzano <ibarram@ugto.mx>
2025-09-16 15:08:28 -0600

    Update

---
+++ src/20253/C/libarram.h
@@ -0,0 +1,15 @@
+#ifndef LIBARRAM_H
+#define LIBARRAM_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#define SWAP(a, b) {\
+		a^=b;\
+		b^=a;\
+		a^=b;\
+		}
+
+#endif // LIBARRAM_H
