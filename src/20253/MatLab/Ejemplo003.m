6f8fecb49f9a8bc75dc5a63005969b53efe7598b
ibarram <ibarram@mac.com>
2025-11-27 12:06:18 -0600

    Update

--- src/20253/MatLab/Ejemplo003.m
+++ src/20253/MatLab/Ejemplo003.m
@@ -19,26 +19,67 @@ end
 
 imgRGGB = imread([path_img file_img]);
 [lv, lu] = size(imgRGGB);
-imgRGB = zeros(lv, lu, 3, 'uint8');
+
+%% Proximos vecinos
+imgRGB1 = zeros(lv, lu, 3, 'uint8');
 
 % Rojo
-imgRGB(1:2:lv, 1:2:lu, 1) = imgRGGB(1:2:lv, 1:2:lu);
-imgRGB(2:2:lv, 1:2:lu, 1) = imgRGGB(1:2:lv, 1:2:lu);
-imgRGB(1:2:lv, 2:2:lu, 1) = imgRGGB(1:2:lv, 1:2:lu);
-imgRGB(2:2:lv, 2:2:lu, 1) = imgRGGB(1:2:lv, 1:2:lu);
+imgRGB1(1:2:lv, 1:2:lu, 1) = imgRGGB(1:2:lv, 1:2:lu);
+imgRGB1(2:2:lv, 1:2:lu, 1) = imgRGGB(1:2:lv, 1:2:lu);
+imgRGB1(1:2:lv, 2:2:lu, 1) = imgRGGB(1:2:lv, 1:2:lu);
+imgRGB1(2:2:lv, 2:2:lu, 1) = imgRGGB(1:2:lv, 1:2:lu);
 
 % Verde
-imgRGB(2:2:lv, 1:2:lu, 2) = imgRGGB(2:2:lv, 1:2:lu);
-imgRGB(1:2:lv, 2:2:lu, 2) = imgRGGB(1:2:lv, 2:2:lu);
-imgRGB(2:2:lv, 2:2:lu, 2) = imgRGGB(2:2:lv, 1:2:lu);
-imgRGB(1:2:lv, 1:2:lu, 2) = imgRGGB(1:2:lv, 2:2:lu);
+imgRGB1(2:2:lv, 1:2:lu, 2) = imgRGGB(2:2:lv, 1:2:lu);
+imgRGB1(1:2:lv, 2:2:lu, 2) = imgRGGB(1:2:lv, 2:2:lu);
+imgRGB1(2:2:lv, 2:2:lu, 2) = imgRGGB(2:2:lv, 1:2:lu);
+imgRGB1(1:2:lv, 1:2:lu, 2) = imgRGGB(1:2:lv, 2:2:lu);
 
 % Azul
-imgRGB(2:2:lv, 2:2:lu, 3) = imgRGGB(2:2:lv, 2:2:lu);
-imgRGB(1:2:lv, 1:2:lu, 3) = imgRGGB(2:2:lv, 2:2:lu);
-imgRGB(2:2:lv, 1:2:lu, 3) = imgRGGB(2:2:lv, 2:2:lu);
-imgRGB(1:2:lv, 2:2:lu, 3) = imgRGGB(2:2:lv, 2:2:lu);
+imgRGB1(2:2:lv, 2:2:lu, 3) = imgRGGB(2:2:lv, 2:2:lu);
+imgRGB1(1:2:lv, 1:2:lu, 3) = imgRGGB(2:2:lv, 2:2:lu);
+imgRGB1(2:2:lv, 1:2:lu, 3) = imgRGGB(2:2:lv, 2:2:lu);
+imgRGB1(1:2:lv, 2:2:lu, 3) = imgRGGB(2:2:lv, 2:2:lu);
 
-figure;
-imshow(imgRGB);
+figure(1);
+imshow(imgRGGB);
 
+figure(2);
+imshow(imgRGB1);
+
+%% Lineal
+imgRGB2 = zeros(lv/2, lu/2, 3, 'uint8');
+
+imgRGB2(:,:,1) = imgRGGB(1:2:lv, 1:2:lu);
+imgRGB2(:,:,2) = (double(imgRGGB(1:2:lv, 2:2:lu))+double(imgRGGB(2:2:lv, 1:2:lu)))/2;
+imgRGB2(:,:,3) = imgRGGB(2:2:lv, 2:2:lu);
+
+figure(3);
+imshow(imgRGB2);
+
+%% Lineal sin reduccion
+imgRGB3 = zeros(lv, lu, 3, 'uint8');
+
+imgRGGB_d = double(imgRGGB);
+indu1 = 1:2:lu;
+indu2 = 2:2:lu;
+indv1 = 1:2:lv;
+indv2 = 2:2:lv;
+
+% Originales de RGGB a RGB
+imgRGB3(indv1, indu1, 1) = imgRGGB_d(indv1, indu1);
+imgRGB3(indv1, indu2, 2) = imgRGGB_d(indv1, indu2);
+imgRGB3(indv2, indu1, 2) = imgRGGB_d(indv2, indu1);
+imgRGB3(indv2, indu2, 3) = imgRGGB_d(indv2, indu2);
+
+imgRGB3(indv2(1:end-1), indu1, 1) = (imgRGGB_d(indv1(1:end-1),indu1)+imgRGGB_d(indv1(1:end-1)+2,indu1))/2;
+imgRGB3(:,2:2:lu-2,1) = (double(imgRGB3(:,1:2:lu-2,1))+double(imgRGB3(:,3:2:lu,1)))/2;
+
+imgRGB3(3:2:lv-1,1:2:lu,2) = (imgRGGB_d(2:2:lv-2,1:2:lu)+imgRGGB_d(4:2:lv, 1:2:lu))/2;
+imgRGB3(2:2:lv-1,2:2:lu,2) = (imgRGGB_d(1:2:lv-2,2:2:lu)+imgRGGB_d(3:2:lv, 2:2:lu))/2;
+
+imgRGB3(3:2:lv-1, 2:2:lu, 3) = (imgRGGB_d(2:2:lv-2,2:2:lu)+imgRGGB_d(4:2:lv,2:2:lu))/2;
+imgRGB3(:,3:2:lu-1, 3) = (double(imgRGB3(:, 2:2:lu-2, 3))+double(imgRGB3(:, 4:2:lu, 3)))/2;
+
+figure(3);
+imshow(imgRGB3);
